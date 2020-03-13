50923c92306175ce447d9f13b6610c46aa294144
HyopangWorld <hyopangworld@gmail.com>
2020-03-13 14:58:38 +0900

    fixed #1 [COM] init setting - 버전 설정 - 프로젝트 구조 설정 - git ignore 및 pod 설정 - swift lint 설정

---
+++ AnimalSound/AnimalSound/Source/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimalSound
+//
+//  Created by 김효원 on 13/03/2020.
+//  Copyright © 2020 김효원. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .purple
+    }
+
+}
---
+++ AnimalSound/Pods/Target Support Files/Pods-AnimalSound/Pods-AnimalSound-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AnimalSound : NSObject
+@end
+@implementation PodsDummy_Pods_AnimalSound
+@end
